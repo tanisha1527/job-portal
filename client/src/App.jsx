745f3e010eef759decec4f142ae459af855658a9
Tanisha <vermatanisha620@gmail.com>
2025-06-03 19:55:21 +0530

    Set up React Router & added context provider with routes for Home, ApplyJob, & Applications pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React from "react"
+import { Route, Routes } from 'react-router-dom'
+import Home from "./pages/Home";
+import ApplyJob from "./pages/ApplyJob";
+import Applications from "./pages/Applications";
 
 const App = () => {
   return (
-    <div>App</div>
-  );
-};
+    <div>
+        <Routes>
+           <Route path="/" element={<Home/>} />
+           <Route path="/apply-job/:id" element={<ApplyJob/>} />
+           <Route path="/applications" element={<Applications/>} />
+        </Routes>
+    </div>
+  )
+}
 
 export default App;
