3d4c0198d36fb175a4eee4a083426f6cb036a1c3
Tanisha <vermatanisha620@gmail.com>
2025-06-07 01:40:17 +0530

    add related jobs section, footer, smooth scroll, and rich-text styles

--- client/src/pages/ApplyJob.jsx
+++ client/src/pages/ApplyJob.jsx
@@ -1,11 +1,13 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { AppContext } from '../context/AppContext'
-import { assets, jobsData } from '../assets/assets'
+import { assets } from '../assets/assets'
 import Loading from '../components/Loading'
 import Navbar from '../components/Navbar'
 import kconvert from 'k-convert'
 import moment from 'moment'
+import JobCard from '../components/JobCard'
+import Footer from '../components/Footer'
 
 const ApplyJob = () => {
 
@@ -74,10 +76,19 @@ const ApplyJob = () => {
                  <div className='rich-text' dangerouslySetInnerHTML={{__html:jobData.description}}></div>
                  <button className='bg-blue-600 p-2.5 px-10 text-white rounded mt-10'>Apply Now</button>
                </div>
+               {/* Right section */}
+               <div className='w-full lg:w-1/3 mt-8 lg:mt-0 lg:ml-8 space-y-5'>
+                 <h2>More jobs from {jobData.companyId.name}</h2>
+                 {jobs.filter(job => job._id !== jobData._id && job.companyId._id === jobData.companyId._id)
+                 .filter(job => true).slice(0,4)
+                 .map((job,index)=> <JobCard key={index} job={job} />)}
+               </div>
+
             </div>
 
            </div>
        </div>
+       <Footer />
     </>
   ) : (
      <Loading />
