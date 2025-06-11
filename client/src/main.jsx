4c26d44f04396f85ddc4f248021a9176475df2b1
Tanisha <vermatanisha620@gmail.com>
2025-06-11 20:01:31 +0530

    change clerk publishable key

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -15,6 +15,7 @@ if (!PUBLISHABLE_KEY) {
   throw new Error('Missing Publishable Key')
 }
 
+
 createRoot(document.getElementById('root')).render(
   <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl='/'>
   <BrowserRouter>
