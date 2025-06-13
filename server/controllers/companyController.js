30470fdb1d9a7e95d29be714e5a19120abf94d80
Tanisha <vermatanisha620@gmail.com>
2025-06-13 20:49:11 +0530

    include applicant count in job listings response

--- server/controllers/companyController.js
+++ server/controllers/companyController.js
@@ -3,6 +3,7 @@ import bcrypt from "bcrypt"
 import { v2 as cloudinary } from "cloudinary"
 import generateToken from "../utils/generateToken.js"
 import Job from "../models/Job.js"
+import JobApplication from "../models/JobApplication.js"
 
 // Register a new company
 export const registerCompany = async (req, res) => {
@@ -146,9 +147,13 @@ export const getCompanyPostedJobs = async (req, res) => {
 
         const jobs = await Job.find({companyId})
 
-        // (ToDo) Adding No. of applicants info in data
+        // Adding No. of applicants info in data
+        const jobsData = await Promise.all(jobs.map(async (job) => {
+              const applicants = await JobApplication.find({jobId: job._id})
+              return {...job.toObject(),applicants:applicants.length}
+        }))
 
-        res.json({success:true, jobsData: jobs})
+        res.json({success:true, jobsData })
         
     } catch (error) {
          res.json({success:false,message:error.message})
