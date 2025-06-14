243a9f60eee665d93143aa710a0fb0a7ea2d9cc6
Tanisha <vermatanisha620@gmail.com>
2025-06-14 15:01:45 +0530

    handle empty states for applicants/jobs and enhance job listing UI

--- client/src/pages/Applications.jsx
+++ client/src/pages/Applications.jsx
@@ -71,7 +71,7 @@ const Applications = () => {
                    <button onClick={updateResume} className='bg-green-100 border border-green-400 rounded-lg px-4 py-2 cursor-pointer'>Save</button>
               </>
               : <div className='flex gap-2'>
-                <a href="" className='bg-blue-100 text-blue-600 px-4 py-2 rounded-lg'>
+                <a target='_blank' href={userData?.resume} className='bg-blue-100 text-blue-600 px-4 py-2 rounded-lg'>
                     Resume
                 </a>
                 <button onClick={()=> setIsEdit(true)} className='text-gray-500 border border-gray-300 rounded-lg px-4 py-2 cursor-pointer'>
